cc1105112e7edd10bf15a45caf47e4c6d6baf493
Vikram Choudhry <vikramchoudhry2@gmail.com>
2025-10-05 22:24:09 -0700

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico?v=2" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/WeGotNext-logo.png?v=2" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/WeGotNext-logo.png?v=2" />
+        <link rel="apple-touch-icon" href="/assets/WeGotNext-logo.png?v=2" />
+        <meta name="theme-color" content="#ff6b35" />
+      </Head>
       <body>
         <Main />
         <NextScript />
